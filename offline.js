90d20b1e2cf7b7337a885f759b72316932ecbdc4
Unknown <punto.damar2@gmail.com>
2018-11-05 15:59:27 +0700

    game

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1541408308,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ground.png",
+		"images/fg.png",
+		"images/bg1.png",
+		"images/bg2.png",
+		"images/bg3.png",
+		"images/sky.png",
+		"images/sign-sheet0.png",
+		"images/plane-sheet0.png",
+		"images/smoke.png",
+		"images/cloud-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
